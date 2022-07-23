957114a98167fe40e3f85cd1299de36c208893dd
digitalsquid7 <jpedler96@outlook.com>
2022-07-23 14:35:37 +1000

    cryptographically secure random identifier for temperatures

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,4 +1,4 @@
-USE global_temperatures
+USE global_temperature
 
 IF OBJECT_ID('dbo.daily_average_temperature', 'U') IS NOT NULL
    DROP TABLE daily_average_temperature
@@ -8,6 +8,36 @@ IF OBJECT_ID('dbo.city', 'U') IS NOT NULL
 
 IF OBJECT_ID('dbo.country', 'U') IS NOT NULL
    DROP TABLE country
+
+IF OBJECT_ID('v_random', 'V') IS NOT NULL
+   DROP VIEW v_random
+
+DROP FUNCTION IF EXISTS dbo.generate_random_unique_value
+
+GO
+
+CREATE VIEW v_random
+AS
+SELECT randval = CONVERT(NVARCHAR(48), CRYPT_GEN_RANDOM(12), 2)
+
+GO
+
+CREATE FUNCTION generate_random_unique_value()
+RETURNS NVARCHAR(48) AS
+BEGIN
+    DECLARE @random_value NVARCHAR(48)
+    DECLARE @row_number INT
+    WHILE 1 = 1
+    BEGIN
+        SELECT @random_value = randval FROM v_random
+        SELECT @row_number = COUNT(1) FROM daily_average_temperature WHERE random_identifier = @random_value
+        IF @row_number = 0
+            BREAK
+    END
+    RETURN @random_value
+END
+
+GO
 
 CREATE TABLE country
 (
@@ -30,6 +60,7 @@ CREATE TABLE city
 CREATE TABLE daily_average_temperature
 (
     id INT IDENTITY(1,1),
+    random_identifier NVARCHAR(48) NOT NULL DEFAULT dbo.generate_random_unique_value(),
     city_id INT NOT NULL,
     temperature_recorded_date DATE NOT NULL,
     average_temperature DECIMAL(5, 3) NOT NULL,
@@ -39,7 +70,8 @@ CREATE TABLE daily_average_temperature
 )
 
 CREATE UNIQUE INDEX uq__country__country_name ON country (country_name)
-CREATE UNIQUE INDEX uq__city__city_name ON city (city_name)
+CREATE INDEX ix__city__city_name ON city (city_name)
 CREATE INDEX ix__city__country_id ON city (country_id)
+CREATE UNIQUE INDEX uq__daily_average_temperature__random_identifier ON daily_average_temperature (random_identifier)
 CREATE INDEX ix__daily_average_temperature__city_id ON daily_average_temperature (city_id)
 CREATE INDEX ix__daily_average_temperature__average_temperature ON daily_average_temperature (average_temperature)
